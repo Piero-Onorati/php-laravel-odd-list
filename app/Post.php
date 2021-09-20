9daf72e886f90161a2dad7ccdd67874513e4224b
Piero <piero.onorati@virgilio.it>
2021-09-20 16:03:18 +0200

    first commit

---
+++ app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable=[
+        'title',
+        'slug',
+        'content',
+        'category_id'
+    ];
+
+    // One to Many relation
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+
+    // Many to Many relation
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
+    }
+}
---
+++ resources/js/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+    ],
+});
+
+export default router;
